da0dacd360c194ae03b148bc31b18ff35548d49e
Martin Vium <martin@codefuss.com>
2022-12-02 10:07:17 +0100

    Day 1

--- day1/main.rb
+++ day1/main.rb
@@ -1,6 +1,31 @@
-def main
-  input = File.read(File.join(File.dirname(__FILE__), "input.txt"))
-  puts input
+# frozen_string_literal: true
+
+require "ap"
+
+class Day1
+  class << self
+    def first
+      elfs.first
+    end
+
+    def second
+      elfs.take(3).sum
+    end
+
+    private
+
+    def elfs
+      input.split("\n\n").map { |elf| elf.split("\n").sum(&:to_i) }.sort.reverse
+    end
+
+    def input
+      File.read(File.join(File.dirname(__FILE__), "input.txt"))
+    end
+  end
 end
 
-main()
+ap "First"
+ap Day1.first
+
+ap "Second"
+ap Day1.second
---
+++ day1/main_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe User do
+  subject(:action) { described_class.new }
+
+  describe "#call" do
+  end
+end
